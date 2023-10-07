a9359f586753b058208829fa9d88d03118e0f19e
RaMadaSilva <raul01mateia@gmail.com>
2023-10-07 15:32:25 +0100

    refatorando a implementação do RealizadorRepository

--- CadastroFilmes.Infra/Repositories/RealizadorRepository.cs
+++ CadastroFilmes.Infra/Repositories/RealizadorRepository.cs
@@ -5,28 +5,14 @@ using Microsoft.EntityFrameworkCore;
 
 namespace CadastroFilmes.Infra.Repositories
 {
-    public class RealizadorRepository : IRealizadorRepository
+    public class RealizadorRepository : BaseRepository<Realizador>,IRealizadorRepository
     {
         private readonly CadastroFilmesDbContext _context;
 
         public RealizadorRepository(CadastroFilmesDbContext context)
+            :base(context)
         {
             _context = context;
-        }
-
-        public async Task AddRealizador(Realizador realizador)
-        {
-            await _context.Realizadores.AddAsync(realizador);
-        }
-
-        public void DeleteRealizador(Realizador realizador)
-        {
-            _context.Realizadores.Remove(realizador);
-        }
-
-        public async Task<IEnumerable<Realizador>> GetRealizadoresAsync()
-        {
-            return await _context.Realizadores.AsNoTracking().ToListAsync();
         }
 
         public async Task<Realizador> GetRealizadorByFilmesAsync(int id)
@@ -34,17 +20,6 @@ namespace CadastroFilmes.Infra.Repositories
             return await _context.Realizadores
                     .Include(x => x.Filmes)
                     .FirstOrDefaultAsync(x => x.Id == id); 
-        }
-
-        public async Task<Realizador> GetRealizadorByIdAsync(int id)
-        {
-            return await _context.Realizadores.FirstOrDefaultAsync(x => x.Id == id); 
-        }
-
-        public void UpdateRealizador(Realizador realizador)
-        {
-            _context.Realizadores.Update(realizador);
-
         }
     }
 }

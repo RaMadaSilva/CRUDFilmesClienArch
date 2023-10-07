708cf3347bd36a4a8058f3e5bbd86097f2253b78
RaMadaSilva <raul01mateia@gmail.com>
2023-10-07 14:08:21 +0100

    refatorando a Interface do repositorio de Filme e de Realizador

--- CadastroFilmes.Domain/IRepositories/IFilmeRepository.cs
+++ CadastroFilmes.Domain/IRepositories/IFilmeRepository.cs
@@ -2,13 +2,8 @@
 
 namespace CadastroFilmes.Domain.IRepositories
 {
-    public interface IFilmeRepository
+    public interface IFilmeRepository: IBaseRepository<Filme>
     {
-        Task<IEnumerable<Filme>> GetFilmesAsync();
-        Task<Filme> GetFilmeByIdAsync(int id);
         Task<IEnumerable<Filme>> GetFilmesWithRealizadoresAsync();
-        Task AddFilme(Filme filme);
-        void UpdateFilme(Filme filme);
-        void DeleteFilme(Filme filme); 
     }
 }
--- CadastroFilmes.Domain/IRepositories/IRealizadorRepository.cs
+++ CadastroFilmes.Domain/IRepositories/IRealizadorRepository.cs
@@ -2,13 +2,8 @@
 
 namespace CadastroFilmes.Domain.IRepositories
 {
-    public interface IRealizadorRepository
+    public interface IRealizadorRepository: IBaseRepository<Realizador>
     {
-        Task<IEnumerable<Realizador>> GetRealizadoresAsync();
-        Task<Realizador> GetRealizadorByIdAsync(int id);
         Task<Realizador> GetRealizadorByFilmesAsync(int id);
-        Task AddRealizador(Realizador realizador);
-        void UpdateRealizador(Realizador realizador);
-        void DeleteRealizador(Realizador realizador);
     }
 }

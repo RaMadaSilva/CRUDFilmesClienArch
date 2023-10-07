b45148e359c7508ad868767d6761892f7ca41a3d
RaMadaSilva <raul01mateia@gmail.com>
2023-10-07 14:04:25 +0100

    Adicionando um repositorio base

---
+++ CadastroFilmes.Domain/IRepositories/IBaseRepository.cs
@@ -0,0 +1,13 @@
+﻿using CadastroFilmes.Domain.Entities;
+
+namespace CadastroFilmes.Domain.IRepositories
+{
+    public interface IBaseRepository<TEntity> where TEntity : Entity
+    {
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<TEntity> GetByIdAsync(int id);
+        Task AddAsync(TEntity entity);
+        Task UpdateAsync(TEntity entity); 
+        Task DeleteAsync(int id);
+    }
+}
